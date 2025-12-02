b2e3928d6bf20abaa23e84bc8fd4ceb34f34964f
Beyza Taştan <bbeyzatastan@gmail.com>
2025-12-02 16:39:42 +0300

    initial commit

---
+++ salary-service/src/test/java/com/beyzatastan/salary_service/SalaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyzatastan.salary_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
